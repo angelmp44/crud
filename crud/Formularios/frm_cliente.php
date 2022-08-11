e76fc7ee4f26157ac7f7fcd3d61ae123f4c59d3a
angelmp44 <miguel.kb10@gmail.com>
2022-08-11 10:40:11 -0500

    fondo
    fondo

--- crud/Formularios/frm_cliente.php
+++ crud/Formularios/frm_cliente.php
@@ -14,9 +14,21 @@
     <link rel="stylesheet" type="text/css" href="../CSS/styles_frm_envios_venta.css">
     <title>Registro Cliente | H&D Construequipos</title>
    
+<!---------------------- Header-------------------------->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+<?php require "../partes/header.html" ?>
+
+<!---------------------- Header-------------------------->
+<!-- menu-->
+<?php require "../partes/navbaragendas.html" ?> 
+<!-- menu-->
 
 </head>
 <body>
+
     
     <form action="registroCliente.php" method="post" class="formulario">  
 
@@ -64,6 +76,9 @@
 
         
     </form> 
-    <img src="../CSS/img/Logo.png" style="width: 150px">
+    
 </body>
+<!-- Footer-->
+<?php require "../partes/footer.html" ?>
+<!-- Footer-->
 </html>
