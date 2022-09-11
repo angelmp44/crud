2e8108192cb79d16e7c087a375993e76d9ad3b01
griverab <69488534+GustavoR-ivera@users.noreply.github.com>
2022-09-11 11:34:56 -0500

    Modificacion de codigo_proveedor
    al modificar el codigo del proveedor se afectan los productos asociados a él.

--- crud/Formularios/actualizarproveedor.php
+++ crud/Formularios/actualizarproveedor.php
@@ -1,8 +1,8 @@
 <?php
-$conexion =new mysqli("localhost","root","","construequipos")
-			or die("no hay conexion".mysqli_connect_error());
-
+require "../conexion.php";
 extract($_REQUEST);
+
+$objConexion = conectarse();
 
 	if (isset($_GET['id_proveedor'])) {
 		// con el metodo get se obtiene el id de proveedor actual
@@ -14,7 +14,15 @@ extract($_REQUEST);
 		$correo_proveedor=$_POST['correo_proveedor'];
         $direccion_proveedor=$_POST['direccion_proveedor'];
 
-	$sql ="UPDATE `proveedor` SET 
+		// crear nuevo registro de proveedor con los datos nuevos (solo si el id:proveedor es nuevo)
+		$sql1 = "INSERT INTO proveedor 
+		(id_proveedor, nombre_proveedor, telefono_proveedor, correo_proveedor, direccion_proveedor)
+        VALUES 
+		('$id_proveedorNuevo', '$nombre_proveedor', '$telefono_proveedor', 
+		'$correo_proveedor', '$direccion_proveedor' )";
+	
+		// actualizacion de proveedor (solo si el id:proveedor es el mismo)
+		$sql2 ="UPDATE `proveedor` SET 
 			`id_proveedor` = '$id_proveedorNuevo',
 			`nombre_proveedor` = '$nombre_proveedor', 
 			`telefono_proveedor` = '$telefono_proveedor', 
@@ -22,6 +30,17 @@ extract($_REQUEST);
             `direccion_proveedor` = '$direccion_proveedor'
 			 
 			WHERE `proveedor`.`id_proveedor` = $id_proveedorAntiguo";
+
+		// actualizacion de productos
+		$sql3 = "UPDATE producto 
+				 SET id_proveedor = '$id_proveedorNuevo'
+				 WHERE producto.id_proveedor = $id_proveedorAntiguo  ";
+
+		// verificacion de codigo de proveedor
+		$sql4 = "SELECT * FROM proveedor WHERE id_proveedor = $id_proveedorNuevo";
+
+		// eliminacion de proveedor antiguo
+		$sql5 = "DELETE FROM proveedor WHERE id_proveedor = $id_proveedorAntiguo";
 
 	
 
@@ -36,22 +55,55 @@ if(empty($id_proveedorNuevo) || empty($_REQUEST['nombre_proveedor'])
 }
 else{
 
-	// antes de ejecutar la consulta, verificar que el id nuevo no exista ya en la bd (proveedor registrado)
-	// actualizar los productos asociados a ese proveedor
+	// verificar si el id nuevo existe ya en la bd (no cambio el id_proveedor)
+	$resultado1 = $objConexion->query($sql4);  
 
-	$query =mysqli_query($conexion,$sql);
-	if($query){
-		echo "<script>
-		alert('Actualización Exitosa');
-		window.location='./mostrarproveedor.php'
-	   </script>";
-	}else{
-		echo mysqli_error($conexion);
-		
+	$cantidad_registros = mysqli_num_rows($resultado1);  /* devuelve la cantidad de registros encontrados */
+
+	/* si cantidad_registros = 1 entonces el proveedor tiene el mismo id*/
+	if($cantidad_registros != 0 ) {
+		$resultado2 = $objConexion->query($sql2);
+	
+		if($resultado2){
+			echo "<script>
+			alert('Actualización de datos de proveedor exitosa');
+			window.location='./mostrarproveedor.php'
+		   </script>";
+		}else{
+			echo "<script>
+			alert('Actualización de datos incorrecta');
+			window.location='./mostrarproveedor.php'
+		   </script>";
+			
+			}
 		}
+	else{
+		// crear nuevo proveedor
+		$resultado3 = $objConexion->query($sql1);
+		// actualizamos productos con el id_proveedor nuevo
+		$resultado4 = $objConexion->query($sql3);
+		// eliminar proveedor antiguo
+		$resultado5 = $objConexion->query($sql5);
 	
-	}
-	
+		if($resultado3 && $resultado4 && $resultado5){
+			echo "<script>
+			alert('Actualización Exitosa');
+			window.location='./mostrarproveedor.php'
+		   </script>";
+		}else{
+			echo "<script>
+			alert('Actualización Incorrecta, inconvenientes con id_proveedorNuevo');
+			window.location='./mostrarproveedor.php'
+		   </script>";
+			
+			}	
+	}	
+  }	
 }
-	
+else{
+	echo "<script>
+			alert('Actualización Incorrecta');
+			window.location='./mostrarproveedor.php'
+		   </script>";
+}
 ?>
